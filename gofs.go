2ca683e33093a00d5b8fe9354096ce2c0f83a31a
Brenton Morris <brenton.morris@odinsol.com>
2015-06-08 20:47:18 +1200

    Made gofs into a proper command line tool.

--- gofs.go
+++ gofs.go
@@ -1,10 +1,37 @@
 package main
 
 import (
+	"os"
 	"net/http"
+	"fmt"
+	"strconv"
+)
+
+const (
+	defaultAddr = ":8888"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./public/")))
-	http.ListenAndServe(":8080", nil)
+	var addr string
+	dir, err := os.Getwd()
+	if err != nil {
+		os.Exit(1)
+	}
+	if len(os.Args) > 1 {
+		addr = os.Args[1]
+		// check address format
+		c := string([]rune(addr)[0])
+		if _, err := strconv.Atoi(c); err == nil {
+			if c != ":" {
+				addr = ":" + addr
+			}
+		}
+	} else {
+		addr = defaultAddr
+	}
+
+	fmt.Printf("Serving files from %s over http on %s\n", dir, addr)
+	http.Handle("/", http.FileServer(http.Dir(dir)))
+	http.ListenAndServe(addr, nil)
 }
+
